9b2cbf51cf05cb0fe057493cc61a38b81700712a
Crowdbotics <engineering@crowdbotics.com>
2023-03-22 13:34:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "crimson-bread-dev-71396",
+  url: "https://crimson-bread-dev-71396.botics.co",
+  api: "https://crimson-bread-dev-71396.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
